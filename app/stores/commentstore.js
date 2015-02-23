3f146ca8d6d6a55de6c2568425a24d6cf703bb83
scneptune <rmalcome@gmail.com>
2015-02-23 12:21:04 -0800

    moved the configStore into the Commentstore for readability and centralization of data.

--- app/stores/commentstore.js
+++ app/stores/commentstore.js
@@ -1,16 +1,27 @@
-var Flux = require('flux-react'),
+var flux = require('flux-react'),
 	actionComment = require('../actions/commentactions'),
 	configStore = require('./configstore.js'),
 	request = require('superagent'),
 	validate = require('validate.js');
 
-var CommentStore = Flux.createStore({
+var CommentStore = flux.createStore({
 	data: [],
 	replyId: null,
 	errors: {},
+	config: {
+		rating: true,
+		commentsNumber: 6,
+		fiveRateNumber: 30,
+		voteRateTotal: 45,
+		albumArt: 'http://hiphopdx.local/s/img/album_thumbnail.png',
+		commentsUrl: 'http://localhost:8000/commentdata.json',
+		pageNumber: 0,
+		postId: 1118,
+		sortBy: 'latest'
+	},
 	actions: [
 		actionComment.addComment,
-		actionComment.validateComment
+		actionComment.validateComment,
 		actionComment.addReply,
 		actionComment.setReplyId,
 		actionComment.formErrors
@@ -19,18 +30,20 @@ var CommentStore = Flux.createStore({
 		self = this;
 		request.get(configStore.config.commentsUrl).set('Accept', 'application/json').end(function(res){
 			self.data.push(res.body)});
-		});
-	}
-	addComment: function (commentObj) {	
-	var comments = this.data;
-		this.errors = this.validateComment(commentObj);
-		this.emit('errors.updated');
-		// if (this.errors.code == 0) {
-			comments.push(commentObj);
-			this.emit('comment.added');
-		// } 
+	},
+	populateConfig: function () {
+		//TODO: move the configs of the json out to this list. or we can technically grab it from the document as an embed json object. 
+	},
+	objectFindByKey: function (array, key, value) {
+	    for (var i = 0; i < array.length; i++) {
+	        if (array[i][key] === value) {
+	            return array[i];
+	        }
+	    }
+	    return null;
 	},
 	validateComment: function (commentObj) {
+		this.errors = {};
 		//run throught a validator to check the input out.
 		var constraints = {
 			'author' : {presence: true, length: {minimum: 2, maximum: 20} },
@@ -48,8 +61,33 @@ var CommentStore = Flux.createStore({
 			return;
 		}
 	},
+	addComment: function (commentObj) {	
+	var comments = this.data;
+		this.errors = this.validateComment(commentObj);
+		this.emit('errors.updated');
+		// if (this.errors.code == 0) {
+			comments.push(commentObj);
+			this.emit('comment.added');
+		// } 
+	},
+	addReply: function (commentObj, replyId) {
+		var comments= this.data,
+			chosenReply = this.objectFindByKey(comments, 'id', replyId);
+		this.errors = this.validateComment(commentObj);
+		this.emit('errors.updated');
+		chosenReply.replies.push(commentObj);
+		this.emit('comment.replyadded');
+	},
 	exports: {
-		data : data
+		getCommentsList: function() {
+			return this.data;
+		},
+		getReplyId: function () {
+			return this.replyId;
+		},
+		getConfig : function () {
+		 return this.config; 
+		}
 	} 
 });
 
